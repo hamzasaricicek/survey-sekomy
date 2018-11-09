3d7ec55b2a5aa77c3f3ebb0914298a66a0646da4
Kaan <knsrky@gmail.com>
2018-11-09 20:50:06 +0300

    test

--- survey/settings.py
+++ survey/settings.py
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+ALLOWED_HOSTS = ['sadece9.com', '40.118.86.221']
